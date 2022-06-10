19fa7d3331659673e704470f0255f53c2c2974f9
leuzaoo <leuzaoocontato@gmail.com>
2022-06-10 14:51:40 -0300

    animation title, css main page

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
 // Library
-import { VscSearch } from "react-icons/vsc";
+import { FiSearch } from "react-icons/fi";
 
-
+// Styles
+import "./mainStyle.css";
 
 function App() {
   return (
@@ -9,11 +10,25 @@ function App() {
       <h1 className="title">Buscador de CEP</h1>
 
       <div className="inputContainer">
-        <input className="inputCEP" type="text" placeholder="digite o cep..."></input>
+        <input
+          className="inputCEP"
+          type="text"
+          placeholder="digite o cep..."
+        ></input>
         <button type="submit" className="searchButton">
-          <VscSearch />
+          <FiSearch />
         </button>
       </div>
+
+      <main className="main">
+        <h2 className="cepNumber">CEP: 13205-730</h2>
+        <div className="localItems">
+          <span>Rua: Nome gerado aleatoriamente.</span>
+          <span>Complemento: Complemento gerado aleatoriamente.</span>
+          <span>Bairro: Bairro aleatório.</span>
+          <span>Cidade: Cidade - Estado</span>
+        </div>
+      </main>
     </div>
   );
 }
