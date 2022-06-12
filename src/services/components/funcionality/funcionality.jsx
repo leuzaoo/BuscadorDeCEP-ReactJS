61e31369404b401a0d0388a7eb930bc409e3f2b0
leuzaoo <leuzaoocontato@gmail.com>
2022-06-12 11:08:05 -0300

    created new router -funcionality

--- src/services/components/funcionality/funcionality.jsx
+++ src/services/components/funcionality/funcionality.jsx
@@ -1,7 +1,24 @@
 import React from "react";
+import Header from "../header/header";
+import "./funcionality.css";
 
 function Funcionality() {
-  return <div>hello world</div>;
+  return (
+    <>
+      <div>
+        <Header />
+      </div>
+
+      <div className="container">
+        <div className="card-box">
+          <h2 className="title-name card">Para um bom funcionamento:</h2>
+          <p>Insira somente números;</p>
+          <p>Apenas endereços do Brasil;</p>
+          <p>Não coloque espaço entre os números.</p>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default Funcionality;
