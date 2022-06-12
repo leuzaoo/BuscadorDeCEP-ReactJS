7ceab999c51f73150cd3f3a35389f5b09e7c022c
leuzaoo <leuzaoocontato@gmail.com>
2022-06-12 12:17:55 -0300

    hamburguer menu

--- src/services/components/header/header.jsx
+++ src/services/components/header/header.jsx
@@ -1,25 +1,36 @@
 import "./header.css";
 import { Link } from "react-router-dom";
+import { useState } from "react";
 
 function Header() {
+  const [active, setMode] = useState(false);
+  const ToggleMode = () => {
+    setMode(!active);
+  };
+
   return (
     <>
-      <div className="nav">
-        <div className="logotipo">
-          <p>Logo</p>
-        </div>
-        <div className="nav-itens">
-          <ul>
-            <li>
-              <Link to="/">Buscador</Link>
-            </li>
-            <li>
-              <Link to="/about">Sobre</Link>
-            </li>
-            <li>
-              <Link to="/funcionality">Como funciona</Link>
-            </li>
-          </ul>
+      <div className={active ? "icon iconActive" : "icon"} onClick = {ToggleMode}>
+        <div className="hamburguer hamburguerIcon"></div>
+      </div>
+      <div className={active ? "menu menuOpen" : "menu menuClose"}>
+        <div className="nav">
+          <div className="logotipo">
+            <p>Logo</p>
+          </div>
+          <div className="nav-itens">
+            <ul>
+              <li>
+                <Link className="itens-name" to="/">Buscador</Link>
+              </li>
+              <li>
+                <Link className="itens-name" to="/about">Sobre</Link>
+              </li>
+              <li>
+                <Link className="itens-name" to="/funcionality">Como funciona</Link>
+              </li>
+            </ul>
+          </div>
         </div>
       </div>
     </>
